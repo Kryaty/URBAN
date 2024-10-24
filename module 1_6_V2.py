cf1aec1501e38237f25fb114966ebc84bf5b142a
Kryaty <kryaty@gmail.com>
2024-10-24 18:41:06 +0300

    Практическое задание по теме "Переменные" V2

---
+++ module 1_6_V2.py
@@ -0,0 +1,4 @@
+number_of_finished_homeworks = 12
+total_hours_spent= 1.5
+name_of_course = "Python"
+print ("Курс:", name_of_course+"," ,"задач всего:", str (number_of_finished_homeworks)+"," ,"затрачено часов:" , str (total_hours_spent)+"," ,"среднее время выполнения:", str (total_hours_spent/number_of_finished_homeworks),"часа.")
